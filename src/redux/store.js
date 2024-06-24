244ef5122e17c3180d711baf1a26c74dc0667306
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-06-24 18:44:03 +0300

    delete persisit code

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,40 +1,19 @@
 import { configureStore} from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactsSlice";
 import { filtersReducer } from "./filtersSlice";
-import storage from 'redux-persist/lib/storage' 
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist'
 
-const persistConfig = {
-  key: 'contacts',
-  storage,
-}
 
-const contactsPersistedReducer = persistReducer(persistConfig, contactsReducer)
 
 const rootReducer = {
-  contacts: contactsPersistedReducer,
+  contacts: contactsReducer,
   filters: filtersReducer,
 };
 
 
 export const store = configureStore({
     reducer: rootReducer,
-     middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+     
 })
 
-export const persistor = persistStore(store);
+
  
